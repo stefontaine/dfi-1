87c2155da3d28f61e116185f9f42a22805eec461
stefontaine <ste.fontaine1@gmail.com>
2025-02-05 00:00:56 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
+let de = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showString("A")
+    basic.showString("stephane")
+})
+input.onGesture(Gesture.Shake, function () {
+    de = randint(0, 6)
+    basic.showString("" + (de))
 })
 basic.forever(function () {
 	
