5e7387afb7fb16d4ea6ce688b254f68708fc7a9c
stefontaine <ste.fontaine1@gmail.com>
2025-02-05 00:11:12 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
-let de = 0
+let sortie = 0
 input.onButtonPressed(Button.A, function () {
     basic.showString("stephane")
 })
 input.onGesture(Gesture.Shake, function () {
-    de = randint(0, 6)
-    basic.showString("" + (de))
+    sortie = randint(1, 3)
+    if (sortie == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (sortie == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
 })
 basic.forever(function () {
 	
